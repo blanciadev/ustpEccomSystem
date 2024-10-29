a598115b9ad1ae53ea74e551589a3190d5a33c91
Angela Mae Salvaña <120080231+AngelaMaeSalvana@users.noreply.github.com>
2024-10-29 22:58:57 +0800

    transition

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,15 +4,18 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { GoogleOAuthProvider } from "@react-oauth/google"
+import { GoogleOAuthProvider } from "@react-oauth/google";
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <GoogleOAuthProvider clientId='604163930378-hefd54geho5pkurgd149svlovu50j81t.apps.googleusercontent.com'>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>,
+    <BrowserRouter>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </BrowserRouter>
   </GoogleOAuthProvider>
-)
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
