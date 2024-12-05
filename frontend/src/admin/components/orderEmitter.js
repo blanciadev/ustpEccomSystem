ee42ead99d64036acda497b600fdde1aa54d4909
Kurt Bernstein P. Blancia <k.blancia.dev@gmail.com>
2024-12-05 09:08:03 +0800

    recomiit for merging

--- frontend/src/admin/components/orderEmitter.js
+++
@@ -1,5 +0,0 @@
-export const listenForDatabaseChanges = () => {
-    const eventSource = new EventSource('http://localhost:5001/api/events');
-
-    return eventSource;
-};
--- frontend/src/client/components/eventEmitter.js
+++
@@ -1,6 +0,0 @@
-// src/components/eventEmitter.js
-import { EventEmitter } from 'events';
-
-// Export an instance of EventEmitter
-export const cartEventEmitter = new EventEmitter();
-
